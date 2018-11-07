c84fd04787eae97f4c61ce6d0aee9d8f7e637144
Matt Mielke <matt.mielke@digikey.com>
2018-11-07 16:20:43 -0600

    Updating Google Cloud parameters
    I updated the Google Cloud project and need to make the corresponding
    changes to IoT_Sensor_Node_config.h.

--- AVRIoTWGSensorNode/Config/IoT_Sensor_Node_config.h
+++ AVRIoTWGSensorNode/Config/IoT_Sensor_Node_config.h
@@ -46,7 +46,7 @@
 // <s> project id
 // <i> Google Cloud Platform project id
 // <id> project_id
-#define CFG_PROJECT_ID "avr-iot-ac"
+#define CFG_PROJECT_ID "avr-iot-rfid-ac"
 
 // <s> project region
 // <i> Google Cloud Platform project region
@@ -56,7 +56,7 @@
 // <s> registry id
 // <i> Google Cloud Platform registry id
 // <id> registry_id
-#define CFG_REGISTRY_ID "scanner"
+#define CFG_REGISTRY_ID "rfid_readers"
 
 // <s> mqtt host
 // <i> mqtt host address
